9bf7725aa48da91f2b5bc971dbb9e1cb7ad590c8
Govind Bhala <you@example.com>
2020-08-06 19:23:31 -0400

    Adding comments

--- CaptionGenerationServer.py
+++ CaptionGenerationServer.py
@@ -1,18 +1,14 @@
 import os
 import flask
-# import numpy as np
-# import tensorflow as tf
-# import tensorflow.keras as keras
 from flask import session, url_for, jsonify, request, redirect, render_template
 from module.Image_Prediction_GRU import model as model_gru
 from module.ImageCaptioning_Full_script_old import model as model_lstm
 from flask_cors import CORS
 
-# UPLOAD_DIRECTORY = os.path.join(os.getcwd(), "app_uploaded_files")
-UPLOAD_DIRECTORY = "C:\\Users\\Govind\\SML\\Project\\React\\ImageLabelling\\src"
 
-# if not os.path.exists(UPLOAD_DIRECTORY):
-#     os.makedirs(UPLOAD_DIRECTORY)
+## Chang the directory name to the src directory  in UI Components
+UPLOAD_DIRECTORY = "C:\\Users\\Govind\\SML\\Project\\UIComponents\\src"
+
 
 app = flask.Flask(__name__)
 CORS(app)
@@ -21,8 +17,12 @@ app.config["DEBUG"] = True
 
 @app.route('/upload-image', methods=['POST'])
 def upload_image():
+	"""
+	This function recieves post request with image file name and model name as parameters.
+	The right model package is loaded and a prediction is generated on the given input image
+	Sample request API : http://localhost:5000/upload_image
+	"""
     if request.method == "POST":
-        # print(request.files.get('Image'))
         if request.files:
             image = request.files.get('Image')
             model_type = request.args['model']
@@ -32,7 +32,7 @@ def upload_image():
             else:
                 model = model_lstm
             image.save(os.path.join(UPLOAD_DIRECTORY, image.filename))
-            predictions = model(os.path.join(UPLOAD_DIRECTORY, image.filename))#['This', 'is', 'a', 'sample', 'prediction', '<end>']                                          
+            predictions = model(os.path.join(UPLOAD_DIRECTORY, image.filename))
             response = jsonify({
                             'prediction' : ' '.join(predictions[:-1]),
                             'image_path' : os.path.join(UPLOAD_DIRECTORY, image.filename)
@@ -43,18 +43,6 @@ def upload_image():
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response     
 
-
-@app.route('/caption', methods = ['GET'])
-def generate_caption():
-    image_name = request.args['name']
-    image_path = os.path.join(UPLOAD_DIRECTORY, image_name)
-
-    predictions = ['This', 'is', 'a', 'sample', 'prediction', '<end>']#model(image_path)
-
-    return jsonify({
-        'prediction' : ' '.join(predictions[:-1]),
-        'image_path' : os.path.join(UPLOAD_DIRECTORY, image_name)
-    })
 
 if __name__ == '__main__':
     app.run(debug=True)
